a3f5cb90aa6ed76d32a60596c5eb2b8aca80c740
psrajoria <psrajoria@outlook.com>
2021-06-20 23:59:46 +0530

    Model created and migrated

--- src/flights/models.py
+++ src/flights/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Flights(models.Model):
+    origin = models.CharField(max_length=60)
+    destination = models.CharField(max_length=60)
+    duration = models.TimeField(auto_now=False, auto_now_add=False)
