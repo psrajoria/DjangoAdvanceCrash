db39b3c1e5f33ae0deb9e853c065cee148df8d5f
psrajoria <psrajoria@outlook.com>
2021-06-22 00:06:34 +0530

    Admin created

--- src/flights/views.py
+++ src/flights/views.py
@@ -2,7 +2,9 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-
+from .models import Flights
 
 def index(request):
-    return HttpResponse("Hello")
+    return render(request,"flights/index.html",{
+        "flights": Flights.objects.all()
+    })
