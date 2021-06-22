b0932c85f1736705ec0c543f35d2ba42a5c2b044
psrajoria <psrajoria@outlook.com>
2021-06-23 00:58:35 +0530

    Exclusion commited

--- src/flights/views.py
+++ src/flights/views.py
@@ -1,8 +1,10 @@
-from django.http.response import HttpResponse
+from django.http.response import  HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 
 # Create your views here.
-from .models import Flights
+from .models import Flights, Passengers
 
 def index(request):
     return render(request,"flights/index.html",{
@@ -13,5 +15,14 @@ def flight(request, flight_id):
     flight = Flights.objects.get(pk=flight_id)
     return render(request,"flights/flight.html",{
         "flight":flight,
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        ####to get non passengers
+        "non_passengers": Passengers.objects.exclude(flights=flight).all(),
     })
+
+def book(request,flight_id):
+    if request.method=="POST":
+        flight = Flights.objects.get(pk=flight_id)
+        passenger = Passengers.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse('flights:flight', args=(flight.id,)))
