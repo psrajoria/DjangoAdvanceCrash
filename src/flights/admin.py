3f290fc242ee329f857efd2e3261dc8602e030b1
psrajoria <psrajoria@outlook.com>
2021-06-25 00:32:05 +0530

    Customize Admin Page

--- src/flights/admin.py
+++ src/flights/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.db import models
 
 # Register your models here.
 from .models import Flights,Airport,Passengers
 
-admin.site.register(Flights)
+class FlightAdmin(admin.ModelAdmin):
+    list_display=("id","origin","destination","duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal=("flights",)
+
+admin.site.register(Flights, FlightAdmin)
 admin.site.register(Airport)
-admin.site.register(Passengers)
+admin.site.register(Passengers, PassengerAdmin)
